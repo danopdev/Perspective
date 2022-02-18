04e6e6fb9a7089f2816c1a33ab3824ac5398655d
Dan Oprea <dan.oprea@masternaut.com>
2022-02-18 10:35:58 +0100

    Add custom ImageView with zoom and pan support

--- app/src/main/java/com/dan/perspective/MainActivity.kt
+++ app/src/main/java/com/dan/perspective/MainActivity.kt
@@ -115,12 +115,7 @@ class MainActivity : AppCompatActivity() {
                     inputImage = loadImage(uri)
                     outputImage = null
 
-                    val bitmap = matToBitmap(inputImage)
-                    if (null == bitmap) {
-                        binding.imageView.setImageResource(android.R.drawable.ic_menu_report_image)
-                    } else {
-                        binding.imageView.setImageBitmap(bitmap)
-                    }
+                    binding.imageView.setBitmap(matToBitmap(inputImage))
                 }
 
                 BusyDialog.dismiss()
